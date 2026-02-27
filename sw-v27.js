c38e9477e0ebba68dba5f71e653ae0ad9ca6335b
goldorak123 <daniellheureux.dlh49@gmail.com>
2026-02-27 18:11:35 -0500

    simonlero.com v27 - Native navigation fix

---
+++ sw-v27.js
@@ -0,0 +1,24 @@
+const CACHE_NAME = 'simonlero-nav-v27';
+const urlsToCache = [
+    '/livre-accueil-v27.html',
+    '/livre-accueil.html',
+    '/manifest.json',
+    '/icon-192.png',
+    '/icon-512.png'
+];
+
+self.addEventListener('install', event => {
+    self.skipWaiting();
+    event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
+});
+
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k))))
+            .then(() => self.clients.claim())
+    );
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(fetch(event.request).catch(() => caches.match(event.request)));
+});

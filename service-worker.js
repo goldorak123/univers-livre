7a4a12bfec38dd7eacc5b8bf04903f0e8ae8d358
goldorak123 <daniellheureux.dlh49@gmail.com>
2026-02-28 18:44:31 -0500

    Contact pages: Force SW v71 to ensure 60% underline visibility

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = 'simon-lero-v70';
-console.log('SW Loading v70...');
+const CACHE_NAME = 'simon-lero-v71';
+console.log('SW Loading v71...');
 const urlsToCache = [
     '/',
     '/livre-accueil.html',
